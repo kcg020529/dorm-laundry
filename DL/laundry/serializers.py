5f609085b98a75efde746a9543783861a7c9b72b
alwayshyeon19@gmail.com <alwayshyeon19@gmail.com>
2025-05-23 13:38:34 +0900

    [fix] 시간 예약 방식 -> 대기열

--- DL/laundry/serializers.py
+++ DL/laundry/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User, Building, WashingMachine, Reservation
+from .models import User, Building, WashingMachine, WaitList
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,21 +12,17 @@ class BuildingSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class WashingMachineSerializer(serializers.ModelSerializer):
-    building = serializers.CharField(source='building.name')  # 건물 이름(A~E)만 반환
+    building = serializers.CharField(source='building.name')
+    wait_count = serializers.SerializerMethodField()  # ▲ 대기 인원수 필드 추가
 
     class Meta:
         model = WashingMachine
         fields = '__all__'
 
-class ReservationSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-    machine = WashingMachineSerializer()
-    
-    class Meta:
-        model = Reservation
-        fields = '__all__'
+    def get_wait_count(self, obj):
+        return obj.waitlist_set.count()  # ▲ 해당 세탁기 대기열 인원 반환
 
-class CreateReservationSerializer(serializers.ModelSerializer):
+class WaitListSerializer(serializers.ModelSerializer):  # ▲ 대기열 직렬화
     class Meta:
-        model = Reservation
-        fields = ['machine', 'start_time', 'end_time']
+        model = WaitList
+        fields = '__all__'
