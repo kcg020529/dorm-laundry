faac142b868caff8be52439ea4573272e769c48d
Github Hjl1436 <nickyl22@naver.com>
2025-05-24 01:51:02 +0900

    앱 구조 및 모델 추가 생성

---
+++ laundry/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class User(models.Model):
+    student_id = models.CharField(max_length=10, unique=True)
+    is_admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.student_id
+
+class Building(models.Model):
+    name = models.CharField(max_length=1)  # A, B, C, D, E
+
+    def __str__(self):
+        return f"{self.name}동"
+
+class WashingMachine(models.Model):
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    is_in_use = models.BooleanField(default=False)
+    description = models.TextField(default='')  # 새 필드
+
+    def __str__(self):
+        return f"{self.building.name}동 - {self.name}"
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    machine = models.ForeignKey(WashingMachine, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.user.student_id} 예약 {self.machine.name} ({self.start_time} ~ {self.end_time})"
+
