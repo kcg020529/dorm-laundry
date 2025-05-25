15d58797fe81226a70e53471e03103cf913898e6
alwayshyeon19@gmail.com <alwayshyeon19@gmail.com>
2025-05-25 16:06:37 +0900

    Autofield 설정 추가가

--- project/settings.py
+++ project/settings.py
@@ -62,3 +62,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'your_email@gmail.com'
 EMAIL_HOST_PASSWORD = 'your_app_password'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+TIME_ZONE = 'Asia/Seoul'
+USE_TZ = True
