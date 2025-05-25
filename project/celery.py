7a41b29304ba45b1240f4e5e0b8a750bd04b5975
alwayshyeon19@gmail.com <alwayshyeon19@gmail.com>
2025-05-25 16:08:02 +0900

    타임존 연동 코드 작성성

--- project/celery.py
+++ project/celery.py
@@ -1,7 +1,10 @@
 import os
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 app = Celery('project')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+app.conf.timezone = settings.TIME_ZONE
