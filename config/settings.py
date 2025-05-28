0a0a5c116164277e01f7619a8cdf3e7fef340299
alwayshyeon19@gmail.com <alwayshyeon19@gmail.com>
2025-05-29 01:43:59 +0900

    static 경로 변경경

--- config/settings.py
+++ config/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'laundry_app',
-        'USER': 'test',
-        'PASSWORD': 'test',
+        'USER': 'root',
+        'PASSWORD': 'hufs',
         'HOST': 'localhost',
         'PORT': '3306',
     }
@@ -124,8 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join[BASE_DIR, 'static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -140,3 +140,5 @@ LOGIN_URL = '/laundry/login/'
 
 #커스텀 유저 모델 사용
 AUTH_USER_MODEL = 'laundry.user'
+
+ALLOWED_HOSTS = ['*']
