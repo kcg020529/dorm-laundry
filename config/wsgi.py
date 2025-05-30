32bf499352f022b5a3de464ec809eddc0cc938e8
alwayshyeon19@gmail.com <alwayshyeon19@gmail.com>
2025-05-30 17:43:23 +0900

    commit

--- config/wsgi.py
+++ config/wsgi.py
@@ -7,10 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+if not logger.handlers:
+    h = logging.StreamHandler(sys.stdout)
+    h.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+    logger.addHandler(h)
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', "dorm_laundry.config.settings")
 
 application = get_wsgi_application()
